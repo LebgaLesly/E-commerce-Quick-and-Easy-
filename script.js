00dcf2c1c35c85d37622032fd9bbd5de24ae7578
LebgaLesly <lebgalesly1@gmail.com>
2023-07-18 15:16:30 -0700

    Home Page responsive on Medium size devices

---
+++ script.js
@@ -0,0 +1,7 @@
+const openNav = () => {
+    document.getElementById("mobile-nav").style.width = "50%";
+  }
+  
+const closeNav = () => {
+    document.getElementById("mobile-nav").style.width = "0%";
+  }
